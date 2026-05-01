fc62aa104c563da464a8d4a1625df3586eba667f
vihaangatla25-cpu <vihaangatla25@gmail.com>
2026-05-01 15:54:01 +1000

    Add files via upload

---
+++ Break.py
@@ -0,0 +1,11 @@
+#Take user input 
+a = input ("Enter a word: ")
+#Program to check break keyword 
+for i in a: #iterate for loop
+    if (i == 'A'): #codition 1 
+    #display result
+      print ("A is found")
+      break #break statement 
+    else:
+    #display result
+      print ("A is not found")
