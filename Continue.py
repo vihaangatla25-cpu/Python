177c7d23223d016fee2f245befbed642dfbb71d0
vihaangatla25-cpu <vihaangatla25@gmail.com>
2026-05-01 16:00:12 +1000

    Add files via upload

---
+++ Continue.py
@@ -0,0 +1,8 @@
+var = 10        #initiaise
+while var > 0:  #iterate loop
+    var = var - 1
+    if var == 5:    #condition 1
+        continue    #continue statement 
+        #display result 
+    print ('/nCurrent variable value : ', var)
+print ("/nGood bye!")
