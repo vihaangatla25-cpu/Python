8ed25ea98757311fbe626264f566d6a4f933db43
vihaangatla25-cpu <vihaangatla25@gmail.com>
2026-05-05 15:38:15 +1000

    Add files via upload

---
+++ Bye.py
@@ -0,0 +1,11 @@
+valid = False
+while not valid: #using nested while loop
+    try:
+        n=int(input("Enter a number:  "))
+        #enter a even number 
+        while n%==0
+
+            print("bye")
+        valid = True
+    except ValueError:
+        print("Invalid")
