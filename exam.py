e2c150984b84d3823e6906d951a253096e7f6a1b
vihaangatla25-cpu <vihaangatla25@gmail.com>
2026-06-25 16:15:58 +1000

    Add files via upload

---
+++ exam.py
@@ -0,0 +1,12 @@
+class book:
+    def __init__(self_bookname):
+         self.bookname = name 
+
+    def borrow():
+        print(self.bookname, "Book Borrow")
+    
+    def return_book():
+        print(self.bookname, "Book Returned")
+
+    def book():
+        print(self.bookname, "Max books is 3")
