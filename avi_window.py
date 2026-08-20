43c31c6be01a2de6b62752b05e7fa1de219ed799
vihaangatla25-cpu <vihaangatla25@gmail.com>
2026-08-20 16:41:52 +1000

    Add files via upload

---
+++ avi_window.py
@@ -0,0 +1,16 @@
+from tkinter import *
+root = Tk()
+root.geometry("400x300")
+root.title("main")
+def topwin():
+  top = Toplevel()
+  top.geometry("180x100")
+  top.title("toplevel")
+  l2 = Label(root, text="this is toplevel windows")
+  l2.pack()
+  top.mainloop()
+  l = Label(root, text="this is root window")
+  btn = button(root, text="click here to open another window", command=topwin)
+  l.pack()
+  btn.pack()
+  root.mainloop()
